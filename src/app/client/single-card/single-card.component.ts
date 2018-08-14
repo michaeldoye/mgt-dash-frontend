7b7ff81ba8cbbc79c1be99108396d4c50fa1dd1f
michael <michaeldoye@gmail.com>
2018-08-14 16:11:40 +0200

    chore:(update add to deck)

--- src/app/client/single-card/single-card.component.ts
+++ src/app/client/single-card/single-card.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -15,25 +17,50 @@ export class SingleCardComponent implements OnInit {
   public isActive: boolean;
   public imgLoaded: boolean;
 
-  private userDocRef: AngularFirestoreDocument<any>;
+  private userDocRef: AngularFirestoreCollection<any>;
   public userDoc$: Observable<any>;
 
   constructor(
     public auth: AngularFireAuth,
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private sb: MatSnackBar
   ) { }
 
   ngOnInit() {
     this.auth.user.subscribe((user) => {
       if (user) {
-        this.userDocRef = this.afs.doc<any>(`users/${user.uid}`);
-        this.userDoc$ = this.userDocRef.valueChanges();
+        this.userDocRef = this.afs.doc<any>(`users/${user.uid}`).collection('decks');
+        this.userDoc$ = this.userDocRef.snapshotChanges().pipe(
+          map(actions => {
+            return actions.map(a => {
+              const data = a.payload.doc.data();
+              const id = a.payload.doc.id;
+              return { id, ...data };
+            })
+          })
+        )
       }
     });
   }
 
-  addToDeck(index: string, card: any) {
-
+  addToDeck(id: string, card: any) {
+    this.userDocRef.doc(id)
+      .collection('cards')
+      .add(card)
+      .then(() => {
+        this.sb.open(
+          'Card Added!',
+          '',
+          {duration: 5000, horizontalPosition: 'left'}
+        );        
+      })
+      .catch(error => {
+        this.sb.open(
+          `There was a problem adding the card; ${error}`, 
+          '',
+          {duration: 5000, horizontalPosition: 'left'}
+        )
+      });
   }
 
 }
